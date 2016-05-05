6d59323941f92b16014b06c256a677257351a150
Hali <halivelazquez@outlook.com>
2016-05-05 13:36:06 -0500

    modified:   Scr/validator.js

--- Scr/validator.js
+++ Scr/validator.js
@@ -12,3 +12,136 @@ function valphone(phone) {
 	var ts = /^[0-9]{8,15}$/;
 	return ts.test(phone);
 }
+
+function enviarForm() {
+	var vn = false, vm = false, vcap = false, vt = false, vs = false, vcb = false;
+	if (!validate($("#correoE01").val()) || ($("#correoE01").val() !== $("#correoE02").val())) {								
+		$("#Email01").attr("class", "form-group has-feedback has-error");
+		$("#Email02").attr("class", "form-group has-feedback has-error");
+		$("#email01").attr("class", "glyphicon glyphicon-remove form-control-feedback");
+		$("#email02").attr("class", "glyphicon glyphicon-remove form-control-feedback");
+		$("#email03").attr("class", "text-center help-block");
+		if ($("#correoE01").val() !== $("#correoE02").val()) {
+			$("#email03").text("Correos proporcionados no coinciden");
+		}
+		vm = false;
+	}
+	else {
+		$("#Email01").attr("class", "form-group has-feedback has-success");
+		$("#Email02").attr("class", "form-group has-feedback has-success");
+		$("#email01").attr("class", "glyphicon glyphicon-ok form-control-feedback");
+		$("#email02").attr("class", "glyphicon glyphicon-ok form-control-feedback");
+		$("#email03").attr("class", "hidden"); 
+		vm = true;
+	}
+
+	if (!valname($("#nombre").val()) || !valname($("#appat").val()) || !valname($("#apmat").val())) {
+		if (valname($("#nombre").val())) {
+			$("#Nombre").attr("class", "form-group has-feedback has-success");
+			$("#name01").attr("class", "glyphicon glyphicon-ok form-control-feedback");
+		}
+		else {
+			$("#Nombre").attr("class", "form-group has-feedback has-error");
+			$("#name01").attr("class", "glyphicon glyphicon-remove form-control-feedback");
+		}
+
+		if (valname($("#appat").val())) {
+			$("#ApellidoP").attr("class", "form-group has-feedback has-success");
+			$("#app01").attr("class", "glyphicon glyphicon-ok form-control-feedback");
+		}
+		else {
+			$("#ApellidoP").attr("class", "form-group has-feedback has-error");
+			$("#app01").attr("class", "glyphicon glyphicon-remove form-control-feedback");
+		}
+
+		if (valname($("#apmat").val())) {
+			$("#ApellidoM").attr("class", "form-group has-feedback has-success");
+			$("#apm01").attr("class", "glyphicon glyphicon-ok form-control-feedback");
+		}
+		else {
+			$("#ApellidoM").attr("class", "form-group has-feedback has-error");								
+			$("#apm01").attr("class", "glyphicon glyphicon-remove form-control-feedback");
+		}
+
+		$("#apm02").attr("class", "text-center help-block");
+		$("#apm02").text("Formato de nombre o apellido incorrecto");
+		vn = false;
+	}
+	else {
+		$("#Nombre").attr("class", "form-group has-feedback has-success");
+		$("#name01").attr("class", "glyphicon glyphicon-ok form-control-feedback");
+		$("#ApellidoP").attr("class", "form-group has-feedback has-success");
+		$("#app01").attr("class", "glyphicon glyphicon-ok form-control-feedback");
+		$("#ApellidoM").attr("class", "form-group has-feedback has-success");								
+		$("#apm01").attr("class", "glyphicon glyphicon-ok form-control-feedback");
+		$("#apm02").attr("class", "hidden text-center help-block"); 
+		vn = true;
+	}
+
+	if (!valphone($("#telefono").val())) {
+		$("#Telefono").attr("class", "form-group has-feedback has-error");
+		$("#phone01").attr("class", "glyphicon glyphicon-remove form-control-feedback");
+		$("#phone02").attr("class", "text-center help-block");
+		$("#phone02").text("Formato de numero de teléfono incorrecto");
+		vt = false;
+	}
+	else {
+		$("#Telefono").attr("class", "form-group has-feedback has-success");
+		$("#phone01").attr("class", "glyphicon glyphicon-ok form-control-feedback");
+		$("#phone02").attr("class", "hidden text-center help-block");
+		vt = true;
+	}
+
+	if ($("#asunto").val() === "") {
+		$("#Asunto").attr("class", "form-group has-feedback has-error");
+		$("#sub01").attr("class", "glyphicon glyphicon-remove form-control-feedback");
+		vs = false;
+	}
+	else {
+		$("#Asunto").attr("class", "form-group has-feedback has-success");
+		$("#sub01").attr("class", "glyphicon glyphicon-ok form-control-feedback");
+		vs = true;
+	}
+
+	if (grecaptcha.getResponse()) {
+		vcap = true;
+	}
+	else {
+		$("#html_element").attr("style", "border-style: solid; border-width: 2px; border-color: #A94442;")
+		vcap = false;
+	}
+	
+	if ($(".hora01:checkbox:checked").length > 0 && $("#date01").val()) {
+		$("#checkboxes01").removeClass("has-error");
+		vcb = true;		
+	}
+	else {
+		$("#checkboxes01").addClass("has-error");
+		vcb = false;
+	}
+	
+	if ($(".hora02:checkbox:checked").length > 0 && $("#date02").val()) {
+		$("#checkboxes02").removeClass("has-error");
+		vcb = true;		
+	}
+	else {
+		$("#checkboxes02").addClass("has-error");
+		vcb = false;
+	}
+
+	if (vcap && vm && vn && vs && vt && vcb) {
+		$("#formCita").submit();
+	}
+	else {
+		$(window).scrollTop(0);
+		$("#error").modal();
+	}
+}
+
+$("#bb1").click(function() {
+	$("#datet1").data("DateTimePicker").show();
+});
+
+$("#date01").focus(function() {
+	$("#datet1").data("DateTimePicker").show();
+});
