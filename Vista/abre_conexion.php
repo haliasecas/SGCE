07e93bb2ca3670fc2639b9504a3e25268071f8d1
Hali <haliasecas@users.noreply.github.com>
2016-05-09 15:55:45 -0500

    Update abre_conexion.php

--- Vista/abre_conexion.php
+++ Vista/abre_conexion.php
@@ -1,7 +1,3 @@
 <?php 
-<<<<<<< Updated upstream
 	$link = mysqli_connect('localhost','root','','mydb');		
-=======
-	$link = mysqli_connect('localhost','root','6224','mydb');		
->>>>>>> Stashed changes
 ?>
