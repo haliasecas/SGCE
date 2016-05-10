af98bb97e56fe6e28718b9a15e3ecc94e0df1acf
Rodrigo Alvarado Hérnandez <Rodrigo Alvarado Hrnandez>
2016-05-10 12:46:23 -0500

    modified:   Modelo/enviarCorreo.php

--- Modelo/enviarCorreo.php
+++ Modelo/enviarCorreo.php
@@ -1,4 +1,59 @@
 <?php
+function mandarCorreoInforme($departamento,$asunto,$contenido,$email){
+    header("Content-Type: text/html;charset=utf-8");
+    require 'PHPMailerAutoload.php';
+    $mail = new PHPMailer();
+    $mail->IsSMTP();
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = "ssl";
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 465;
+    $mail->SMTPKeepAlive = true;
+    $mail->Username = "sgceescom@gmail.com";
+    $mail->Password = "sgceescom10";
+    $mail->CharSet = 'UTF-8';
+    $mail->From = $email;
+    $mail->FromName = "Estimado ".$nombreEncargado." ".$appatEncargado." ".$apmatEncargado;
+    $mail->Subject = "Informe o sugerencia";
+    $msg="<div class=\"container-fluid\" style=\"padding-bottom:9px;\" id=\"header\">
+            <img src=\"../Img/SEP.png\" height=\"64px\" style=\"float:left; padding-left:15px;\">
+            <img class=\"img-head\" src=\"../Img/logoIPNGris.png\" style=\"float:right; padding-top:15px; padding-right:15px;\">
+        </div><br><br><br><br> <br> ";
+    include("abre_conexion.php"); 
+
+    $id = sprintf("SELECT nombre,appaterno,apmaterno,correo FROM area WHERE idepto='$departamento'");
+    $result=mysqli_query($link,$id);
+    $rowpersonal = mysqli_fetch_assoc($result);
+    $nombreEncargado = $rowpersonal['nombre'];
+    $appatEncargado = $rowpersonal['appaterno'];
+    $apmatEncargado = $rowpersonal['apmaterno'];
+    $correoEncargado = $rowpersonal['correo'];
+
+    $id = sprintf("SELECT nombre FROM depto WHERE idepto='$departamento'");
+    $result2=mysqli_query($link,$id);
+    $rowdepto = mysqli_fetch_assoc($result2);
+    $nombreDepto = $rowdepto['nombre'];
+
+    $msg=$msg."<b>Buen día </b> <br> $nombre $appatEncargado $apmatEncargado.<br>";
+    include("cierra_conexion.php"); 
+    if($asunto=='1')
+        $msg=$msg."Se ha realizado una solicitud de pedir informe <br> En el departamento $nombreDepto <br>";
+    else
+        $msg=$msg."Se ha realizado una solicitud de sugerencia <br> En el departamento $nombreDepto <br>";
+    $msg=$msg."<b>Enviada desde el correo: $email</b><br>";
+    $msg=$msg."Asunto: ".$asunto."<br>";
+    $msg=$msg."Contenido: ".$contenido."<br>";
+    $msg=$msg."Préstele mucha atención a esta solicitud, puesto que nuestro principal objetivo es la comodidad de nuestros clientes, y así prestar un mejor servicio.<br>";
+    $mail->MsgHTML($msg);
+    $mail->AddAddress($correoEncargado, "Holaaaaaaaa");
+    $mail->IsHTML(true);
+    if(!$mail->Send()) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
 function mandarCorreoSolicitud($nombre,$appat,$apmat,$email,$stringtoken) {
 	header("Content-Type: text/html;charset=utf-8");
 	require 'PHPMailerAutoload.php';
