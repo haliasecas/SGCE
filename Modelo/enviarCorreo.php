9c04f92c1d35f969c8c75acb10e45d3afeb272c2
Rodrigo Alvarado Hérnandez <Rodrigo Alvarado Hrnandez>
2016-05-16 19:55:06 -0500

    modified:   Modelo/enviarCorreo.php

--- Modelo/enviarCorreo.php
+++ Modelo/enviarCorreo.php
@@ -79,7 +79,7 @@ function mandarCorreoSolicitud($nombre,$appat,$apmat,$email,$stringtoken) {
 	$msg="<div class=\"container-fluid\" style=\"padding-bottom:9px;\" id=\"header\">
 			<img src=\"../Img/SEP.png\" height=\"64px\" style=\"float:left; padding-left:15px;\">
 			<img class=\"img-head\" src=\"../Img/logoIPNGris.png\" style=\"float:right; padding-top:15px; padding-right:15px;\">
-		</div><br><br><br><br> <br> <b>Buen día </b> <br> ".$nombre." ".$appat." ".$apmat."<br> <b>Haz pedido agendar una cita el/los día(s)</b><br>El dia: ".$_POST['date01']."<br>Con horarios:<br>";
+		</div><br><br><br><br> <br> <b>Buen día </b> <br> ".$nombre." ".$appat." ".$apmat."<br> <b>Has pedido agendar una cita el/los día(s)</b><br>El dia: ".$_POST['date01']."<br>Con horarios:<br>";
 	$horarios = array(' ','9:00-10:00','13:00-14:00','20:00-21:00','10:00-11:00','14:00-15:00','11:00-12:00','18:00-19:00','12:00-13:00','19:00-20:00');
 	if(!empty($_POST['hora01'])){
 		foreach($_POST['hora01'] as $selected){
