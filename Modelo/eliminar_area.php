4762e78ab63bce28268ec1aae167d4810a85ae9a
NatanLycan <nathaniel981@gmail.com>
2016-05-21 18:10:44 -0500

    Vamo a borrar areas

--- Modelo/eliminar_area.php
+++ Modelo/eliminar_area.php
@@ -1,10 +1,114 @@
-<?php
-        include("abre_conexion.php");
-            //Recibimos por POST los datos procedentes del formulario          
-            if(isset($_GET["id"]))
-	           $id = $_GET['id'];
-                $query1 = "delete FROM depto d, area a WHERE a.idarea = '$id' and a.iddepto=d.iddepto";
-                mysqli_query($link, $query1);            
-                include("cierra_conexion.php");       
-                header("refresh: 1; url = ../Vista/AdministrarAreas.php");//Linea de reedirección
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SGCE</title>
+        <meta charset="utf-8">
+        <script type="text/javascript" src="../Scr/jquery-2.2.0.js"></script>
+        <script type="text/javascript" src="../Scr/moment.min.js"></script>
+        <script type="text/javascript" src="../Scr/bootstrap.js"></script>
+        <script type="text/javascript" src="../Scr/bootstrap-datetimepicker.js"></script>
+        <script type="text/javascript" src="../Scr/validator.js"></script>
+        <link type="text/css" rel="stylesheet" href="../Css/bootstrap.css">
+        <link type="text/css" rel="stylesheet" href="../Css/letras.css">
+        <link type="text/css" rel="stylesheet" href="../Css/modals.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+
+    <body>
+
+        <?php
+        if(isset($_GET["id"]))
+            $id = $_GET['id'];
+        ?>
+
+
+        <div class="modal fade" data-keyboard="false" data-backdrop="static" id="dropfail" role="dialog">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header modal-has-warning">
+                        <h4 class="modal-title">Mensaje de alerta</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>Algunas solicitudes dependendientes de esta área seran eliminadas tambien. 
+                            ¿Esta seguro de que desea eliminar el registro?</p>
+                    </div>
+                    <div class="modal-footer">
+
+                        <button type='button' class='btn btn-warning' data-dismiss='modal' onclick="window.location =<?php echo"'../Modelo/eliminar_area.php?id=$id&lol=1'" ?>;">
+                            Si
+                        </button>
+
+                        <button type="button" class="btn btn-warning" data-dismiss="modal" onclick="window.location = '../Vista/AdministrarAreas.php';">
+                            No
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal fade" data-keyboard="false" data-backdrop="static" id="drop" role="dialog">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header modal-has-success">
+                        <h4 class="modal-title">Mensaje de notificación</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p>Se han eliminado los registros exitosamente</p>
+                    </div>
+                    <div class="modal-footer">
+
+                        <button type="button" class="btn btn-success" data-dismiss="modal" onclick="window.location = '../Vista/AdministrarAreas.php';">
+                            Aceptar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <?php
+    include("abre_conexion.php");
+                                //Recibimos por POST los datos procedentes del formulario          
+
+
+                                if(isset($_GET["lol"])){
+                                    $lol = $_GET["lol"];
+                                    //echo $lol;
+                                    $query = "delete from solicitud where idarea = $id";
+                                    mysqli_query($link, $query);
+                                    $query1 = "delete FROM area WHERE idarea = $id";
+                                    mysqli_query($link, $query1); 
+
+        ?>
+        <script>
+            $("#drop").modal();
+        </script>
+        <?php
+                                }else{
+                                    //echo "NO hay LOL";
+                                    $query = "select * from solicitud where idarea=$id";
+                                    $result = mysqli_query($link, $query);
+                                    if(($row = mysqli_fetch_array($result, MYSQLI_ASSOC))>0){
+        ?>
+        <script>
+            $("#dropfail").modal();
+        </script>
+        <?php
+
+                                    }else{
+                                        $query1 = "delete FROM area WHERE idarea = $id";
+                                        mysqli_query($link, $query1);           
+        ?>
+        <script>
+            $("#drop").modal();
+        </script>
+        <?php
+
+                                    }
+
+                                }
+
+                                include("cierra_conexion.php");  
+        ?>
+    </body>
+</html>
